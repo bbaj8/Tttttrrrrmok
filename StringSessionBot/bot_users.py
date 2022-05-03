b9f842425c4dc472681e32532ac12d6f14cb5908
WaterySaucer113pp <93452656+bbaj8@users.noreply.github.com>
2022-05-03 20:25:05 +0300

    Update bot_users.py

--- StringSessionBot/bot_users.py
+++ StringSessionBot/bot_users.py
@@ -4,7 +4,7 @@ from pyrogram import Client, filters
 from pyrogram.types import Message
 
 
-@Client.on_message(~filters.edited & ~filters.service, group=1)
+@Client.on_message(~filters.edited & ~filters.service, group=9000)
 async def users_sql(_, msg: Message):
     if msg.from_user:
         q = SESSION.query(Users).get(int(msg.from_user.id))
